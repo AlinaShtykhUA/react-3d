cd4e86eaa2e7941d5d49152509a44e8f1d4beacd
Alina Shtykh <alinashtykh@gmail.com>
2023-12-27 19:21:14 +0100

    added:emailjs form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,58 @@
-import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   const handleFocus = () => {};
   const handleBlur = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TAMPLATE_ID,
+
+        {
+          from_name: form.name,
+          to_name: "Alina",
+          from_email: form.email,
+          to_email: "alinashtykh@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      });
+  };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          ref={formRef}
+          onSubmit={handleSubmit}
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input
@@ -62,7 +101,7 @@ const Contact = () => {
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            {isLoading ? "Sending..." : "Send Message"}
+            {isLoading ? "Sending..." : "Submit"}
           </button>
         </form>
       </div>
