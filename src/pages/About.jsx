e60529d46c4057dce6c382d93669b8d91e81ee68
Alina Shtykh <alinashtykh@gmail.com>
2024-01-13 21:49:22 +0100

    added: experiences info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,11 @@
-import { skills } from "../constants";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import { skills, experiences } from "../constants";
+
+import "react-vertical-timeline-component/style.min.css";
 
 const About = () => {
   return (
@@ -14,11 +21,7 @@ const About = () => {
         <p>
           Front-end developer. In two-year engineering journey I’ve actively
           contributed to diverse projects, showcasing my adaptability and
-          problem-solving skills in web development. Professional experience
-          includes a key role in a large team for a charity foundation's design,
-          and optimizing user interfaces. Freelance improved my ability to
-          manage client communications and project timelines effectively,
-          ensuring high-quality and timely delivery of web solutions.
+          problem-solving skills in web development.
         </p>
       </div>
 
@@ -38,6 +41,60 @@ const About = () => {
               </div>
             </div>
           ))}
+        </div>
+      </div>
+
+      <div className="py-16">
+        <h3 className="subhead-text">Work experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            Professional experience includes a key role in a large team for a
+            charity foundation's design, and optimizing user interfaces.
+            Freelance improved my ability to manage client communications and
+            project timelines effectively, ensuring high-quality and timely
+            delivery of web solutions.
+          </p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {experience.title}
+                  </h3>
+                  <p
+                    className="text-black-500 font-medium font-base"
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
     </section>
