8626e35ed3eeb58b377beb902988c7cb6e11836d
Alina Shtykh <alinashtykh@gmail.com>
2024-01-13 21:57:50 +0100

    added: cta component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,7 @@ import {
 import { skills, experiences } from "../constants";
 
 import "react-vertical-timeline-component/style.min.css";
+import { CTA } from "../components";
 
 const About = () => {
   return (
@@ -99,6 +100,8 @@ const About = () => {
       </div>
 
       <hr className="border-slate-200" />
+
+      <CTA />
     </section>
   );
 };
